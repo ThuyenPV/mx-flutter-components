7dd343094e6500acf7910ff42802ae5de17425e8
William <thuyen_pham@datahouse.com>
2022-07-05 09:57:33 +0700

    feat: add slide transition

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:mx_crypto_ui/mx_crypto_ui.dart';
 import 'package:mx_flutter_components/config/app_router.dart';
 import 'package:mx_flutter_components/dashboard/view/dashboard_screen.dart';
 import 'package:mx_flutter_components/l10n/l10n.dart';
+import 'package:mx_flutter_components/utils/page_route_util.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,6 +14,18 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: AppRouter.allRouter(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case DashboardScreen.route:
+            return PageRouteUtil.slideRouteBuilder(settings, MxCryptoScreen());
+          case MxCryptoScreen.route:
+            return PageRouteUtil.slideRouteBuilder(settings, MxCryptoDetailScreen());
+          case MxCryptoDetailScreen.route:
+            return PageRouteUtil.slideRouteBuilder(settings, DashboardScreen());
+          default:
+            return MaterialPageRoute(builder: (_) => UnknownPage());
+        }
+      },
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
         colorScheme: ColorScheme.fromSwatch(
@@ -24,6 +38,19 @@ class App extends StatelessWidget {
       ],
       supportedLocales: AppLocalizations.supportedLocales,
       home: const DashboardScreen(),
+    );
+  }
+}
+
+class UnknownPage extends StatelessWidget {
+  const UnknownPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Page not found!'),
+      ),
     );
   }
 }
---
+++ lib/utils/page_route_util.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PageRouteUtil {
+  static PageRouteBuilder slideRouteBuilder(RouteSettings? settings, Widget page) {
+    return PageRouteBuilder(
+      settings: settings,
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        final end = Offset.zero;
+        final curve = Curves.ease;
+        final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+}
